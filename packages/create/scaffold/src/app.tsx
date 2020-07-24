2603396f45ac63935a0a8c033d1077a4eb032402
Gokhan Kurt <krtgkn@gmail.com>
2020-07-25 00:13:57 +0300

    update scaffold

--- packages/create/scaffold/src/app.tsx
+++ packages/create/scaffold/src/app.tsx
@@ -11,7 +11,7 @@ class App extends React.Component<{}, {}> {
   render() {
     return <scroll layout={{ FlexDirection: FlexDirection.Column, Wrap: Wrap.Wrap, AlignItems: YogaAlign.FlexStart, Padding: 40, PaddingRight: 0 }}>
       <text layout={{ MaxWidth: 300, MarginRight: 40, FlexShrink: 1, FlexGrow: 1, FlexBasis: YogaValueNative.Percent(60) }} style={{ textOverflow: 'Linked' }}
-      >{`Go to ${`<color=red>app.tsx</color>`} to edit this file`}</text>
+      >{`Go to <color=red>app.tsx</color> to edit this file`}</text>
     </scroll>;
   }
 }
