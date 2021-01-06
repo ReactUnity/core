b2e6d13c3ae8f94a8b681b8699c3791667200a6e
Gokhan Kurt <krtgkn@gmail.com>
2021-01-06 04:26:06 +0300

    update to webpack 5

--- packages/scripts/config/webpack.config.js
+++ packages/scripts/config/webpack.config.js
@@ -614,18 +614,6 @@ module.exports = function (webpackEnv) {
         },
       }),
     ].filter(Boolean),
-    // Some libraries import Node modules but don't use them in the browser.
-    // Tell webpack to provide empty mocks for them so importing them works.
-    node: {
-      module: 'empty',
-      dgram: 'empty',
-      dns: 'mock',
-      fs: 'empty',
-      http2: 'empty',
-      net: 'empty',
-      tls: 'empty',
-      child_process: 'empty',
-    },
     // Turn off performance processing because we utilize
     // our own hints via the FileSizeReporter
     performance: false,
