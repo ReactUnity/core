7ce836639c516ce8fee1edbf443c75906e2e167d
Gokhan Kurt <krtgkn@gmail.com>
2021-02-01 03:12:45 +0300

    add ability to parse multiple comma separated css enums

--- Runtime/Parsers/EnumConverter.cs
+++ Runtime/Parsers/EnumConverter.cs
@@ -15,6 +15,24 @@ namespace ReactUnity.Styling.Parsers
 
         public object FromString(string value)
         {
+            if (value.Contains(","))
+            {
+                var splits = value.Split(',');
+
+                var result = 0;
+
+                for (int i = 0; i < splits.Length; i++)
+                {
+                    var split = splits[i];
+
+                    var splitRes = Enum.Parse(typeof(T), split.Replace("-", "").ToLowerInvariant(), true);
+
+                    result = result | ((int) splitRes);
+                }
+
+                return (T) Enum.ToObject(typeof(T), result);
+            }
+
             if (value != null && Enum.TryParse<T>(value.Replace("-", "").ToLowerInvariant(), true, out var res)) return res;
             return SpecialNames.CantParse;
         }
