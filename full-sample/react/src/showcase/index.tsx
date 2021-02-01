28ed56b5f19b67d0202ca7a0bca440e529545a90
Gokhan Kurt <krtgkn@gmail.com>
2021-02-01 03:13:18 +0300

    add basic showcase

--- full-sample/react/src/showcase/index.tsx
+++ full-sample/react/src/showcase/index.tsx
@@ -1,7 +1,12 @@
-import { ReactUnity, Slider } from '@reactunity/renderer';
+import { Dropdown, DropdownItem, ReactUnity, Slider, Tooltip } from '@reactunity/renderer';
 import style from './index.module.scss';
 
 export function App() {
+  const tooltipContent =
+    <view style={{ padding: 10, backgroundColor: 0.4, color: 'white' }}>
+      Cool tooltip
+    </view>;
+
   return <view className={style.app}>
     <h1>React Unity Showcase</h1>
 
@@ -20,6 +25,26 @@ export function App() {
         <toggle />
         Toggle
       </row>
+    </section>
+
+    <section>
+      <h2>Tooltip</h2>
+
+      <Tooltip tooltipContent={tooltipContent} position='bottom' offset={20}>
+        Hover to see cool tooltip.
+      </Tooltip>
+    </section>
+
+    <section>
+      <h2>Dropdown</h2>
+
+      <Dropdown>
+        Select an option
+
+        <DropdownItem>Normal Option</DropdownItem>
+        <DropdownItem triggerTemplate={<view style={{ color: 'green' }}>Green Option</view>}>Green Option 💚</DropdownItem>
+        <DropdownItem triggerTemplate={<view style={{ color: 'red' }}>Red Option</view>}>Red Option 🧡</DropdownItem>
+      </Dropdown>
     </section>
 
 
