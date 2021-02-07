b0f8d50faa7961486ae37a9ba372a1b36de36813
Gokhan Kurt <krtgkn@gmail.com>
2021-02-07 16:08:02 +0300

    refactor ReactUnityRenderer -> Renderer

--- packages/create/scaffold/src/app.tsx
+++ packages/create/scaffold/src/app.tsx
@@ -1,18 +1,11 @@
 import * as React from 'react';
-import { ReactUnity, FlexDirection, Wrap, YogaAlign } from '@reactunity/renderer';
+import { Renderer, FlexDirection, Wrap, YogaAlign } from '@reactunity/renderer';
 
-class App extends React.Component<{}, {}> {
-
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return <scroll layout={{ FlexDirection: FlexDirection.Column, Wrap: Wrap.Wrap, AlignItems: YogaAlign.FlexStart, Padding: 40, PaddingRight: 0 }}>
-      <text layout={{ MaxWidth: 300, MarginRight: 40, FlexShrink: 1, FlexGrow: 1, FlexBasis: YogaValueNative.Percent(60) }} style={{ textOverflow: 'Linked' }}
-      >{`Go to <color=red>app.tsx</color> to edit this file`}</text>
-    </scroll>;
-  }
+function App {
+  return <scroll layout={{ FlexDirection: FlexDirection.Column, Wrap: Wrap.Wrap, AlignItems: YogaAlign.FlexStart, Padding: 40, PaddingRight: 0 }}>
+    <text layout={{ MaxWidth: 300, MarginRight: 40, FlexShrink: 1, FlexGrow: 1, FlexBasis: YogaValueNative.Percent(60) }} style={{ textOverflow: 'Linked' }}
+    >{`Go to <color=red>app.tsx</color> to edit this file`}</text>
+  </scroll>;
 }
-ReactUnity.render(<App />, RootContainer, null);
+
+Renderer.render(<App />, RootContainer, null);
