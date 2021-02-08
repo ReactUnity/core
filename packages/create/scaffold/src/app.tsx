60eb792d74a3cc96fb01a32eb3026ceeb6a21337
Gokhan Kurt <krtgkn@gmail.com>
2021-02-08 22:24:03 +0300

    add css file

--- packages/create/scaffold/src/app.tsx
+++ packages/create/scaffold/src/app.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { Renderer, FlexDirection, Wrap, YogaAlign } from '@reactunity/renderer';
+import { Renderer } from '@reactunity/renderer';
+import './index.css';
 
-function App {
-  return <scroll layout={{ FlexDirection: FlexDirection.Column, Wrap: Wrap.Wrap, AlignItems: YogaAlign.FlexStart, Padding: 40, PaddingRight: 0 }}>
-    <text layout={{ MaxWidth: 300, MarginRight: 40, FlexShrink: 1, FlexGrow: 1, FlexBasis: YogaValueNative.Percent(60) }} style={{ textOverflow: 'Linked' }}
-    >{`Go to <color=red>app.tsx</color> to edit this file`}</text>
+function App() {
+  return <scroll>
+    <text>{`Go to <color=red>app.tsx</color> to edit this file`}</text>
   </scroll>;
 }
 
