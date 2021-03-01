853a3848bde0e1c73d5908e9a896a70f9e58e06c
Gokhan Kurt <krtgkn@gmail.com>
2021-03-01 04:09:28 +0300

    added basic editor uielements

--- packages/renderer/models/editor/jsx.ts
+++ packages/renderer/models/editor/jsx.ts
@@ -8,6 +8,8 @@ type Children<T = any> = { children?: T };
 type Textable = string | number | boolean | null | undefined;
 
 
+type BaseElement = Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
+
 import Cmp = ReactUnity.Editor.Renderer.Components;
 
 declare module 'react/jsx-runtime' {
@@ -21,25 +23,60 @@ declare module 'react/jsx-runtime' {
     }
 
     interface IntrinsicElements {
-      [key: string]: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
-      view: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & { tag?: string } & Children;
+      [key: string]: BaseElement;
+      view: BaseElement & { tag?: string };
       anchor: Components.Anchor & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
       text: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children<Textable | Textable[]>;
       button: Components.Button & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
-      scroll: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
+      scroll: BaseElement;
       image: Components.Image & rc.RefAttributes<Cmp.EditorReactComponent> & Children<never>;
       toggle: Components.Toggle & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
-      helpbox: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
-      foldout: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
-      popup: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
-      slider: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
-      stepper: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
-      range: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
-      repeat: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
-      scroller: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
-      list: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
-      imgui: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
-      template: Components.View & rc.RefAttributes<Cmp.EditorReactComponent> & Children;
+      helpbox: BaseElement;
+      foldout: BaseElement;
+      popup: BaseElement;
+      slider: BaseElement;
+      sliderint: BaseElement;
+      range: BaseElement;
+      repeat: BaseElement;
+      scroller: BaseElement;
+      list: BaseElement;
+      imgui: BaseElement;
+      template: BaseElement;
+
+      color: BaseElement;
+      bounds: BaseElement;
+      boundsint: BaseElement;
+      curve: BaseElement;
+      double: BaseElement;
+      enum: BaseElement;
+      flags: BaseElement;
+      float: BaseElement;
+      gradient: BaseElement;
+      inspector: BaseElement;
+      integer: BaseElement;
+      layer: BaseElement;
+      layermask: BaseElement;
+      long: BaseElement;
+      mask: BaseElement;
+      object: BaseElement;
+      progress: BaseElement;
+      property: BaseElement;
+      rect: BaseElement;
+      rectint: BaseElement;
+      tag: BaseElement;
+      vector2: BaseElement;
+      vector2int: BaseElement;
+      vector3: BaseElement;
+      vector3int: BaseElement;
+      vector4: BaseElement;
+      toolbar: BaseElement;
+      'tb-breadcrumbs': BaseElement;
+      'tb-button': BaseElement;
+      'tb-menu': BaseElement;
+      'tb-popupsearch': BaseElement;
+      'tb-search': BaseElement;
+      'tb-spacer': BaseElement;
+      'tb-toggle': BaseElement;
     }
   }
 }
