52a3249cd86a1ca08cbb2d415a768421683f4106
Gokhan Kurt <krtgkn@gmail.com>
2021-03-09 00:54:35 +0300

    add example editor view

--- full-sample/react-editor/src/app.tsx
+++ full-sample/react-editor/src/app.tsx
@@ -1,52 +1,70 @@
-import { Renderer } from '@reactunity/renderer/editor';
-import { useEffect, useState } from 'react';
-import './index.module.scss';
+import { ReactUnity, Renderer } from '@reactunity/renderer/editor';
+import { useMemo, useRef } from 'react';
+import style from './index.module.scss';
+
+const dd = UnityEditor.DragAndDrop;
+
+let startPos = UnityEngine.Vector3.zero;
+let curPos = UnityEngine.Vector3.zero;
 
 export function App() {
-  const [ct, setCt] = useState(0);
+  const ongui = useMemo(() => {
 
-  const a = 5;
+    return () => {
 
-  useEffect(() => {
-    setInterval(() => setCt(x => x + 1), 1000);
-  }, [setCt]);
+      var y = im.current.Element.worldBound.yMin;
+      var st = new UnityEngine.Vector3(startPos.x, startPos.y - y, 0);
+      var en = new UnityEngine.Vector3(curPos.x, curPos.y - y, 0);
 
-  const ongui = () => {
-    if (UnityEngine.GUILayout.Button("I am a button", UnityEngine.GUILayout.Width(150))) {
-      console.log("You clicked the button!");
-    }
+      var tanDist = new UnityEngine.Vector3(st.x - en.x, st.y - en.y, 0).magnitude / 2;
 
+      var stan = new UnityEngine.Vector3(st.x + tanDist, st.y, 0);
+      var etan = new UnityEngine.Vector3(en.x + tanDist, en.y, 0);
 
-    const startPoint = new UnityEngine.Vector3(-0.0, 0.0, 0.0);
-    const endPoint = new UnityEngine.Vector3(50.0, 50.0, 0.0);
+      UnityEditor.Handles.DrawBezier(st, en, stan, etan, UnityEngine.Color.yellow, null, 4);
+    };
+  }, []);
 
-    UnityEditor.Handles.color = UnityEngine.Color.black;
+  const boxes = [0, 1, 2, 3];
 
-    UnityEditor.Handles.DrawLine(startPoint, endPoint, 5);
-  };
+  const im = useRef<ReactUnity.Editor.Renderer.Components.EditorIMGUIComponent>();
 
-  return <view style={{ flexGrow: 1 }}>
+  return <view style={{ flexGrow: 1 }} onDragUpdated={(ev) => {
+    curPos = new UnityEngine.Vector3(ev.mousePosition.x, ev.mousePosition.y, 0);
+    im.current?.MarkDirtyRepaint();
+  }}>
+    <imgui onGUI={ongui} className={style.canvas} ref={im} />
 
-    <toolbar>
-      <tb-button>Button</tb-button>
-
-      <tb-popupsearch></tb-popupsearch>
-
-      <tb-toggle>Toggle</tb-toggle>
-
-      <tb-breadcrumbs>My bread crumbs</tb-breadcrumbs>
-    </toolbar>
-
-    Hello world
-
-    <view onClick={() => console.log('Clickff')} onContextClick={() => console.log('Context click')}
-      onFocus={() => console.log('Selected')} onBlur={() => console.log('Deselected')} focusable>
-      My name is {ct}
-      A good framework
-    </view>
-
-    <imgui onGUI={ongui} />
+    {boxes.map(b => <Box id={b} key={b} />)}
   </view>;
 };
+
+export function Box({ id }: { id: number, key: any }) {
+  return <view className={style.box} style={{ left: id * 102, top: id * 42 }}>
+    This is a box
+
+    <view className={`${style.handle} handle`} onPointerDown={(ev, sender) => {
+      dd.PrepareStartDrag();
+      dd.SetGenericData('id', id as any);
+      dd.StartDrag('Title');
+      curPos = startPos = new UnityEngine.Vector3(ev.position.x, ev.position.y, 0);
+    }} onMouseUp={(ev, sender) => {
+      sender.ReleaseMouse();
+    }} onMouseMove={(ev, sender) => {
+      if (sender.HasMouseCapture()) {
+        curPos = new UnityEngine.Vector3(ev.mousePosition.x, ev.mousePosition.y, 0);
+      }
+    }} onDragUpdated={(ev) => {
+      curPos = new UnityEngine.Vector3(ev.mousePosition.x, ev.mousePosition.y, 0);
+      if (dd.GetGenericData('id') === id as any)
+        dd.visualMode = UnityEditor.DragAndDropVisualMode.Rejected;
+      else dd.visualMode = UnityEditor.DragAndDropVisualMode.Link;
+    }} onDragPerform={() => {
+      console.log(`Incoming ID ${dd.GetGenericData('id')}. This ID ${id}.`);
+      dd.AcceptDrag();
+    }} />
+  </view>;
+}
+
 
 Renderer.render(<App />);
