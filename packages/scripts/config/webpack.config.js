26e3ab5b04bc1518b21e6057c9acb21be65d7869
Gokhan Kurt <krtgkn@gmail.com>
2021-04-12 01:16:26 +0300

    use react-app eslint config

--- packages/scripts/config/webpack.config.js
+++ packages/scripts/config/webpack.config.js
@@ -596,7 +596,7 @@ module.exports = function (webpackEnv) {
         cwd: paths.appPath,
         resolvePluginsRelativeTo: __dirname,
         baseConfig: {
-          extends: [require.resolve('eslint-config-react-app/base')],
+          extends: [require.resolve('eslint-config-react-app')],
           rules: {
             ...(!hasJsxRuntime && {
               'react/react-in-jsx-scope': 'error',
