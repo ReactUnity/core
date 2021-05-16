b96c6d4f4a4bd38bd5a6a376fbb3d36fa0417f38
Gokhan Kurt <krtgkn@gmail.com>
2021-05-16 21:26:23 +0300

    add more media query values

--- Runtime/StyleEngine/MediaProvider.cs
+++ Runtime/StyleEngine/MediaProvider.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace ReactUnity.StyleEngine
 {
@@ -13,6 +14,7 @@ namespace ReactUnity.StyleEngine
         void SetValue(string property, string value);
         void SetNumber(string property, float value);
         void SetDimensions(float width, float height);
+        void RecalculateDefaults();
     }
 
     public class DefaultMediaProvider : IMediaProvider
@@ -31,7 +33,129 @@ namespace ReactUnity.StyleEngine
         Dictionary<string, float> numbers;
         Dictionary<string, string> values;
 
-        public event System.Action<IMediaProvider> OnUpdate;
+        public event Action<IMediaProvider> OnUpdate;
+
+        public DefaultMediaProvider(string mediaType, Dictionary<string, float> numbers = null, Dictionary<string, string> values = null)
+        {
+            MediaType = mediaType;
+            this.numbers = numbers ?? new Dictionary<string, float>();
+            this.values = values ?? new Dictionary<string, string>();
+
+            RecalculateDefaults();
+        }
+
+
+        public void RecalculateDefaults()
+        {
+            var updated = false;
+
+
+            float oldVal = GetNumericalValue("screen-width");
+            float newVal = Screen.width;
+
+            if (oldVal != newVal)
+            {
+                numbers["screen-width"] = newVal;
+                updated = true;
+            }
+
+
+
+            oldVal = GetNumericalValue("screen-height");
+            newVal = Screen.height;
+
+            if (oldVal != newVal)
+            {
+                numbers["screen-height"] = newVal;
+                updated = true;
+            }
+
+
+            oldVal = GetNumericalValue("screen-dpi");
+            newVal = Screen.dpi;
+
+            if (oldVal != newVal)
+            {
+                numbers["screen-dpi"] = newVal;
+                updated = true;
+            }
+
+
+            oldVal = GetNumericalValue("resolution-width");
+            newVal = Screen.currentResolution.width;
+
+            if (oldVal != newVal)
+            {
+                numbers["resolution-width"] = newVal;
+                updated = true;
+            }
+
+
+
+            oldVal = GetNumericalValue("resolution-height");
+            newVal = Screen.currentResolution.height;
+
+            if (oldVal != newVal)
+            {
+                numbers["resolution-height"] = newVal;
+                updated = true;
+            }
+
+
+            oldVal = GetNumericalValue("screen-brightness");
+            newVal = Screen.brightness;
+
+            if (oldVal != newVal)
+            {
+                numbers["screen-brightness"] = newVal;
+                updated = true;
+            }
+
+
+            oldVal = GetNumericalValue("screen-refresh-rate");
+            newVal = Screen.currentResolution.refreshRate;
+
+            if (oldVal != newVal)
+            {
+                numbers["screen-refresh-rate"] = newVal;
+                updated = true;
+            }
+
+
+            var oldBool = GetValue("full-screen") != null;
+            var newBool = Screen.fullScreen;
+
+            if (oldBool != newBool)
+            {
+                values["full-screen"] = newBool ? "yes" : null;
+                updated = true;
+            }
+
+
+
+            oldBool = GetValue("cursor") != null;
+            newBool = Cursor.visible;
+
+            if (oldBool != newBool)
+            {
+                values["cursor"] = newBool ? "visible" : "hidden";
+                updated = true;
+            }
+
+
+
+            var oldLock = GetValue("cursor-lock");
+            var newLock = Cursor.lockState == CursorLockMode.Confined ? "confined" : Cursor.lockState == CursorLockMode.Locked ? "locked" : "none";
+
+            if (oldLock != newLock)
+            {
+                values["cursor-lock"] = newLock;
+                updated = true;
+            }
+
+
+            if (updated) OnUpdate?.Invoke(this);
+        }
 
         public float GetNumericalValue(string property)
         {
@@ -46,32 +170,20 @@ namespace ReactUnity.StyleEngine
             return null;
         }
 
-        public DefaultMediaProvider(string mediaType, Dictionary<string, float> numbers = null, Dictionary<string, string> values = null)
-        {
-            MediaType = mediaType;
-            this.numbers = numbers;
-            this.values = values;
-        }
-
         public void SetValue(string property, string value)
         {
-            values ??= new Dictionary<string, string>();
             values[property] = value;
             OnUpdate?.Invoke(this);
         }
 
         public void SetNumber(string property, float value)
         {
-            numbers ??= new Dictionary<string, float>();
             numbers[property] = value;
             OnUpdate?.Invoke(this);
         }
 
         public void SetDimensions(float width, float height)
         {
-            numbers ??= new Dictionary<string, float>();
-            values ??= new Dictionary<string, string>();
-
             numbers["width"] = width;
             numbers["height"] = height;
             numbers["aspect-ratio"] = width / height;
