0b3aa3e1996aead7e7321612643e4a0784606316
Gokhan Kurt <krtgkn@gmail.com>
2021-06-03 02:50:31 +0300

    add keyframes parsing test

---
+++ Tests/Editor/Styling/AnimationTests.cs
@@ -0,0 +1,41 @@
+using ExCSS;
+using NUnit.Framework;
+using System.Linq;
+using UnityEngine;
+
+namespace ReactUnity.Editor.Tests
+{
+    [TestFixture]
+    public class AnimationTests
+    {
+        static StylesheetParser Parser = new StylesheetParser(true, true, true, true, true);
+
+        [Test]
+        public void KeyframesParser()
+        {
+            var parsed = Parser.Parse(@"
+@keyframes appear {
+  from {
+    opacity: 0%;
+    scale: 0.8 0.8;
+  }
+  to {
+    opacity: 100%;
+    scale: 1 1;
+  }
+}
+");
+
+            var kfs = KeyframeList.Create(parsed.Children.OfType<IKeyframesRule>().First());
+
+            Assert.True(kfs.Valid);
+            Assert.AreEqual(kfs.From, kfs.Steps[0]);
+            Assert.AreEqual(kfs.To, kfs.Steps[1]);
+            Assert.AreEqual(0, kfs.From.Rules["opacity"]);
+            Assert.AreEqual(new Vector2(0.8f, 0.8f), kfs.From.Rules["scale"]);
+
+            Assert.AreEqual(1, kfs.To.Rules["opacity"]);
+            Assert.AreEqual(new Vector2(1, 1), kfs.To.Rules["scale"]);
+        }
+    }
+}
