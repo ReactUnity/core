cb0313b59585e08816a501006fb339c9002f8bab
Gokhan Kurt <krtgkn@gmail.com>
2021-07-03 22:05:15 +0300

    improve location

--- Runtime/DomProxies/Location.cs
+++ Runtime/DomProxies/Location.cs
@@ -18,10 +18,9 @@ namespace ReactUnity.DomProxies
 
         ReactContext ctx;
 
-        public Location(ReactContext ctx)
+        public Location(string href, ReactContext ctx = null)
         {
             this.ctx = ctx;
-            var href = ctx.Script.GetResolvedSourceUrl();
 
             var hashSplit = href.Split('#');
             var hashless = hashSplit[0];
@@ -29,21 +28,28 @@ namespace ReactUnity.DomProxies
 
             var searchSplit = hashless.Split('?');
             var search = searchSplit.Length > 1 ? "?" + searchSplit[1] : "";
+            var searchless = searchSplit[0];
 
-            var hrefSplit = href.Split(new string[] { "//" }, 2, StringSplitOptions.None);
+            var hrefSplit = searchless.Split(new string[] { "//" }, 2, StringSplitOptions.None);
 
-            var protocol = hrefSplit.Length > 1 ? hrefSplit.First() : null;
+            var hasProtocol = hrefSplit.Length > 1;
+            var protocol = hasProtocol ? hrefSplit.First() : null;
 
-            var hrefWithoutProtocol = hrefSplit.Length > 1 ? string.Join("", hrefSplit.Skip(1)) : href;
+            var hrefWithoutProtocol = string.Join("//", hrefSplit.Skip(hasProtocol ? 1 : 0));
             var hrefWithoutProtocolSplit = hrefWithoutProtocol.Split(new string[] { "/" }, 2, StringSplitOptions.None);
 
-            var host = hrefWithoutProtocolSplit.FirstOrDefault();
-            var hostSplit = host.Split(new string[] { ":" }, 2, StringSplitOptions.None);
-            var hostName = hostSplit.First();
-            var port = hostSplit.ElementAtOrDefault(1) ?? "";
 
-            var origin = protocol + "//" + host;
-            var pathName = string.Join("", hrefWithoutProtocolSplit.Skip(1));
+            var hostCandidate = hrefWithoutProtocolSplit.FirstOrDefault();
+            var hasHost = hasProtocol || hostCandidate.Contains(":") || (hostCandidate.IndexOf(".") > 0);
+
+
+            var host = hasHost ? hrefWithoutProtocolSplit.FirstOrDefault() : null;
+            var hostSplit = host?.Split(new string[] { ":" }, 2, StringSplitOptions.None);
+            var hostName = hostSplit?.First();
+            var port = hostSplit != null ? (hostSplit.ElementAtOrDefault(1) ?? "") : null;
+
+            var origin = hasHost ? (protocol + "//" + host) : null;
+            var pathName = "/" + string.Join("/", hrefWithoutProtocolSplit.Skip(hasHost ? 1 : 0));
 
             this.href = href;
             this.protocol = protocol;
@@ -54,12 +60,14 @@ namespace ReactUnity.DomProxies
             this.search = search;
             this.hash = hash;
             this.pathname = pathName;
-            this.restart = ctx.OnRestart;
+            this.restart = ctx?.OnRestart;
         }
+
+        public Location(ReactContext ctx) : this(ctx.Script.GetResolvedSourceUrl(), ctx) { }
 
         public void reload()
         {
-            ctx.Dispatcher.OnceUpdate(restart);
+            ctx?.Dispatcher.OnceUpdate(restart);
         }
     }
 }
---
+++ Tests/Editor/Core/LocationTests.cs
@@ -0,0 +1,60 @@
+using NUnit.Framework;
+using ReactUnity.DomProxies;
+
+namespace ReactUnity.Editor.Tests
+{
+    [TestFixture]
+    public class LocationTests
+    {
+        [Test]
+        public void TestFullLocation()
+        {
+            var href = "https://localhost:3000/folder/index.js?param1=5&param2=hello#test-hash";
+            var lc = new Location(href, null);
+
+            Assert.AreEqual(href, lc.href);
+            Assert.AreEqual("https://localhost:3000", lc.origin);
+            Assert.AreEqual("https:", lc.protocol);
+            Assert.AreEqual("localhost:3000", lc.host);
+            Assert.AreEqual("localhost", lc.hostname);
+            Assert.AreEqual("3000", lc.port);
+            Assert.AreEqual("/folder/index.js", lc.pathname);
+            Assert.AreEqual("?param1=5&param2=hello", lc.search);
+            Assert.AreEqual("#test-hash", lc.hash);
+        }
+
+        [Test]
+        public void TestLocationWithoutPath()
+        {
+            var href = "http://localhost:3000";
+            var lc = new Location(href, null);
+
+            Assert.AreEqual(href, lc.href);
+            Assert.AreEqual("http://localhost:3000", lc.origin);
+            Assert.AreEqual("http:", lc.protocol);
+            Assert.AreEqual("localhost:3000", lc.host);
+            Assert.AreEqual("localhost", lc.hostname);
+            Assert.AreEqual("3000", lc.port);
+            Assert.AreEqual("/", lc.pathname);
+            Assert.AreEqual("", lc.search);
+            Assert.AreEqual("", lc.hash);
+        }
+
+        [Test]
+        public void TestLocationWithoutHost()
+        {
+            var href = "react/index.js?param1=5&param2=hello#test-hash";
+            var lc = new Location(href, null);
+
+            Assert.AreEqual(href, lc.href);
+            Assert.AreEqual(null, lc.origin);
+            Assert.AreEqual(null, lc.protocol);
+            Assert.AreEqual(null, lc.host);
+            Assert.AreEqual(null, lc.hostname);
+            Assert.AreEqual(null, lc.port);
+            Assert.AreEqual("/react/index.js", lc.pathname);
+            Assert.AreEqual("?param1=5&param2=hello", lc.search);
+            Assert.AreEqual("#test-hash", lc.hash);
+        }
+    }
+}
