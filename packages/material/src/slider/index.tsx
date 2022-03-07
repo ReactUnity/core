fd3b98751108d970c2c98036dba05aab38c9b120
Gokhan Kurt <krtgkn@gmail.com>
2022-03-07 19:49:24 +0300

    improve Slider like React controlled elements

--- packages/material/src/slider/index.tsx
+++ packages/material/src/slider/index.tsx
@@ -1,22 +1,25 @@
 import { ReactUnity } from '@reactunity/renderer';
 import { AxisEventCallback, PointerEventCallback, View } from '@reactunity/renderer/ugui';
 import clsx from 'clsx';
-import React, { forwardRef, ReactElement, ReactNode, useCallback, useImperativeHandle, useRef, useState } from 'react';
+import React, { forwardRef, ReactNode, useCallback, useImperativeHandle, useLayoutEffect, useRef, useState } from 'react';
+import { useAutoRef } from '../util/hooks/use-auto-ref';
+import { ControlProps, ControlRef, useControlCheck } from '../util/hooks/use-control-check';
 import style from './index.module.scss';
 
 export type SliderDirection = 'horizontal' | 'vertical' | 'horizontal-reverse' | 'vertical-reverse';
 export type SliderMode = 'normal' | 'diff' | 'falloff';
 export type SliderChildCallback = ((val: number) => ReactNode);
 
-export interface SliderProps extends View {
-  onChange?: (value: number) => void;
+export interface SliderRef extends ControlRef<number> {
+  root: ReactUnity.UGUI.ContainerComponent;
+}
+
+export interface SliderProps extends View, ControlProps<number> {
   min?: number;
   max?: number;
-  value?: number;
   step?: number;
   keyStep?: number;
   mode?: SliderMode;
-  initialValue?: number;
   allowScroll?: boolean;
   scrollMultiplier?: number;
   direction?: SliderDirection;
@@ -24,8 +27,8 @@ export interface SliderProps extends View {
 }
 
 const SliderChild = forwardRef(
-  function _SliderChild({ callback, initialValue }: { callback: SliderChildCallback, initialValue: number }, ref) {
-    const [st, setSt] = useState(initialValue);
+  function _SliderChild({ callback, defaultValue }: { callback: SliderChildCallback, defaultValue: number }, ref) {
+    const [st, setSt] = useState(defaultValue);
 
     useImperativeHandle(ref, () => setSt);
 
@@ -34,101 +37,117 @@ const SliderChild = forwardRef(
     </>;
   });
 
-function _Slider({
-  onChange, onScroll, name, children, initialValue, value, direction = 'horizontal',
-  mode = 'normal', min = 0, max = 1, step = 0, keyStep = null, allowScroll = false,
-  scrollMultiplier = 1 / 6, ...otherProps
-}: SliderProps): ReactElement {
+const _Slider = forwardRef<SliderRef, SliderProps>(
+  function _Slider({
+    onChange, onScroll, name, children, defaultValue, value, direction = 'horizontal',
+    mode = 'normal', min = 0, max = 1, step = 0, keyStep = null, allowScroll = false,
+    scrollMultiplier = 1 / 6, readOnly, ...otherProps
+  }, ref) {
+    const isControlled = useControlCheck({ value, defaultValue, onChange, readOnly });
+    const init = defaultValue ?? value ?? min;
+    const curValue = useRef(init);
+    const innerValue = useRef(init);
+    const onChangeRef = useAutoRef(onChange);
 
-  const curValue = useRef(initialValue ?? value ?? min);
-  const innerValue = useRef(curValue.current);
+    const orientation = (direction === 'vertical' || direction === 'vertical-reverse') ? 'vertical' : 'horizontal';
+    const isReverse = direction === 'vertical-reverse' || direction === 'horizontal-reverse';
+    const sizeProp = orientation === 'horizontal' ? 'width' : 'height';
+    const coordProp = orientation === 'horizontal' ? 'x' : 'y';
+    const crossCoordProp = orientation === 'horizontal' ? 'y' : 'x';
+    const range = max - min;
 
-  const orientation = (direction === 'vertical' || direction === 'vertical-reverse') ? 'vertical' : 'horizontal';
-  const isReverse = direction === 'vertical-reverse' || direction === 'horizontal-reverse';
-  const sizeProp = orientation === 'horizontal' ? 'width' : 'height';
-  const coordProp = orientation === 'horizontal' ? 'x' : 'y';
-  const crossCoordProp = orientation === 'horizontal' ? 'y' : 'x';
-  const range = max - min;
+    const elementRef = useRef<ReactUnity.UGUI.ContainerComponent>();
+    const fillRef = useRef<ReactUnity.UGUI.ContainerComponent>();
+    const childRef = useRef<(val: number) => void>();
 
-  const ref = useRef<ReactUnity.UGUI.ContainerComponent>();
-  const fillRef = useRef<ReactUnity.UGUI.ContainerComponent>();
-  const childRef = useRef<(val: number) => void>();
+    const moveStep = keyStep || step || (range / 10);
+    const setValWithStep = useCallback((val: number, skipNotify?: boolean, skipControl?: boolean) => {
+      const clampedVal = Math.max(min, Math.min(max, val));
+      const steppedVal = step > 0 ? Math.round(clampedVal / step) * step : clampedVal;
+      const oldValue = curValue.current;
 
-  const moveStep = keyStep || step || (range / 10);
-  const setValWithStep = useCallback((val: number) => {
-    val = Math.max(min, Math.min(max, val));
-    innerValue.current = val;
-    if (step > 0) val = Math.round(val / step) * step;
+      if ((!isControlled && !readOnly) || skipControl) {
+        innerValue.current = clampedVal;
+        curValue.current = steppedVal;
 
-    const oldValue = curValue.current;
-    curValue.current = val;
-    if (oldValue !== val) onChange?.(val);
+        if (fillRef.current) {
+          const ratio = (curValue.current - min) / range;
+          fillRef.current.Style.Set(sizeProp === 'width' ? 'height' : 'width', null);
+          fillRef.current.Style.Set(sizeProp, (ratio * 100) + '%');
+        }
 
-    if (fillRef.current) {
-      const ratio = (curValue.current - min) / range;
-      fillRef.current.Style.Set(sizeProp === 'width' ? 'height' : 'width', null);
-      fillRef.current.Style.Set(sizeProp, (ratio * 100) + '%');
-    }
-
-    childRef.current?.(val);
-  }, [min, max, step, sizeProp, range]);
-
-  const dragCallback: PointerEventCallback = useCallback(ev => {
-    const mul = isReverse ? -1 : 1;
-    let val = innerValue.current;
-
-    if (mode === 'diff' || mode === 'falloff') {
-      const diff = (ev.delta[coordProp] / 200) * range;
-
-      if (mode === 'falloff') {
-        const yDiff = Math.max(Math.abs(ev.pressPosition[crossCoordProp] - ev.position[crossCoordProp]) / 100, 1);
-
-        val += mul * diff / (yDiff * yDiff);
+        childRef.current?.(steppedVal);
       }
-      else val += mul * diff;
-    } else {
-      const relPos = ref.current.GetRelativePosition(ev.position.x, ev.position.y);
-      let relRatio = (relPos[coordProp] / (ref.current.RectTransform.rect[sizeProp]));
-      if ((coordProp === 'x' && isReverse) || (coordProp === 'y' && !isReverse))
-        relRatio = 1 - relRatio;
-      val = relRatio * range + min;
-    }
 
-    setValWithStep(val);
-  }, [innerValue, setValWithStep, mode, coordProp, crossCoordProp, sizeProp, isReverse, range, min]);
+      if (!skipNotify && !readOnly && oldValue !== steppedVal) onChangeRef.current?.(steppedVal);
+    }, [min, max, step, sizeProp, range, isControlled, readOnly]);
 
-  const moveCallback: AxisEventCallback = useCallback((ev) => {
-    let diff = ev.moveVector[coordProp] * moveStep;
-    if (isReverse) diff = -diff;
-    setValWithStep(curValue.current + diff);
-  }, [coordProp, moveStep, isReverse, setValWithStep]);
+    useLayoutEffect(() => {
+      if (isControlled) setValWithStep(value, true, true);
+    }, [value, isControlled]);
 
-  const scrollCallback: PointerEventCallback = useCallback((ev, sender) => {
-    if (allowScroll) {
-      const delta = Math.abs(ev.scrollDelta.y) > Math.abs(ev.scrollDelta.x) ? ev.scrollDelta.y : ev.scrollDelta.x;
-      let diff = delta * moveStep * scrollMultiplier;
+    const dragCallback: PointerEventCallback = useCallback(ev => {
+      const mul = isReverse ? -1 : 1;
+      let val = innerValue.current;
+
+      if (mode === 'diff' || mode === 'falloff') {
+        const diff = (ev.delta[coordProp] / 200) * range;
+
+        if (mode === 'falloff') {
+          const yDiff = Math.max(Math.abs(ev.pressPosition[crossCoordProp] - ev.position[crossCoordProp]) / 100, 1);
+
+          val += mul * diff / (yDiff * yDiff);
+        }
+        else val += mul * diff;
+      } else {
+        const relPos = elementRef.current.GetRelativePosition(ev.position.x, ev.position.y);
+        let relRatio = (relPos[coordProp] / (elementRef.current.RectTransform.rect[sizeProp]));
+        if ((coordProp === 'x' && isReverse) || (coordProp === 'y' && !isReverse))
+          relRatio = 1 - relRatio;
+        val = relRatio * range + min;
+      }
+
+      setValWithStep(val);
+    }, [innerValue, setValWithStep, mode, coordProp, crossCoordProp, sizeProp, isReverse, range, min]);
+
+    const moveCallback: AxisEventCallback = useCallback((ev) => {
+      let diff = ev.moveVector[coordProp] * moveStep;
       if (isReverse) diff = -diff;
       setValWithStep(curValue.current + diff);
-    }
+    }, [coordProp, moveStep, isReverse, setValWithStep]);
 
-    onScroll?.(ev, sender);
-  }, [moveStep, isReverse, setValWithStep, onScroll, allowScroll, scrollMultiplier]);
+    const scrollCallback: PointerEventCallback = useCallback((ev, sender) => {
+      if (allowScroll) {
+        const delta = Math.abs(ev.scrollDelta.y) > Math.abs(ev.scrollDelta.x) ? ev.scrollDelta.y : ev.scrollDelta.x;
+        let diff = delta * moveStep * scrollMultiplier;
+        if (isReverse) diff = -diff;
+        setValWithStep(curValue.current + diff);
+      }
 
-  return <view name={name || '<Slider>'} {...otherProps} ref={ref} data-direction={direction} data-orientation={orientation}
-    onDrag={dragCallback} onPointerClick={dragCallback} onPotentialDrag={dragCallback} onMove={moveCallback} onScroll={scrollCallback}
-    className={clsx(style.host, otherProps.className, 'mat-slider')}>
-    <view name="<Slider-Track>" className={clsx(style.track, 'mat-slider-track')}>
-      <view name="<Slider-Fill>" className={clsx(style.fill, 'mat-slider-fill')} ref={fillRef} style={{ [sizeProp]: (curValue.current - min) / range }}>
-        <view name="<Slider-Thumb-Container>" className={clsx(style.thumbContainer, 'mat-slider-thumb-container')}>
-          <view name="<Slider-Thumb>" className={clsx(style.thumb, 'mat-slider-thumb')}>
-            {typeof children === 'function' ?
-              <SliderChild initialValue={curValue.current} callback={children as any} ref={childRef} /> :
-              children}
+      onScroll?.(ev, sender);
+    }, [moveStep, isReverse, setValWithStep, onScroll, allowScroll, scrollMultiplier]);
+
+    useImperativeHandle(ref, () => ({
+      getValue: () => curValue.current,
+      setValue: setValWithStep,
+      get root() { return elementRef.current; },
+    }), [elementRef, curValue, setValWithStep]);
+
+    return <view name={name || '<Slider>'} {...otherProps} ref={elementRef} data-direction={direction} data-orientation={orientation} data-readonly={readOnly ? true : undefined}
+      onDrag={dragCallback} onPointerClick={dragCallback} onPotentialDrag={dragCallback} onMove={moveCallback} onScroll={scrollCallback}
+      className={clsx(style.host, otherProps.className, 'mat-slider')}>
+      <view name="<Slider-Track>" className={clsx(style.track, 'mat-slider-track')}>
+        <view name="<Slider-Fill>" className={clsx(style.fill, 'mat-slider-fill')} ref={fillRef} style={{ [sizeProp]: (100 * (curValue.current - min) / range) + '%' }}>
+          <view name="<Slider-Thumb-Container>" className={clsx(style.thumbContainer, 'mat-slider-thumb-container')}>
+            <view name="<Slider-Thumb>" className={clsx(style.thumb, 'mat-slider-thumb')}>
+              {typeof children === 'function' ?
+                <SliderChild defaultValue={curValue.current} callback={children as any} ref={childRef} /> :
+                children}
+            </view>
           </view>
         </view>
       </view>
-    </view>
-  </view>;
-}
+    </view>;
+  });
 
 export const Slider = React.memo(_Slider);
---
+++ packages/material/src/util/hooks/use-control-check.ts
@@ -0,0 +1,49 @@
+import { useEffect, useRef } from 'react';
+
+export interface ControlProps<T> {
+  value?: T;
+  defaultValue?: T;
+  onChange?: (value: T) => void;
+  readOnly?: boolean;
+}
+
+export interface ControlRef<T> {
+  getValue: () => T | undefined;
+  setValue: (value: T, skipNotify?: boolean) => void;
+}
+
+const error1 = 'A component is changing an uncontrolled input to be controlled. This is likely caused by the value changing from undefined to a defined value, which should not happen. Decide between using a controlled or uncontrolled input element for the lifetime of the component. More info: https://reactjs.org/link/controlled-components';
+const error2 = 'You provided a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`.';
+const error3 = 'App contains an input element with both value and defaultValue props. Input elements must be either controlled or uncontrolled (specify either the value prop, or the defaultValue prop, but not both). Decide between using a controlled or uncontrolled input element and remove one of these props. More info: https://reactjs.org/link/controlled-components';
+const error4 = 'A component is changing a controlled input to be uncontrolled. This is likely caused by the value changing from a defined to undefined, which should not happen. Decide between using a controlled or uncontrolled input element for the lifetime of the component. More info: https://reactjs.org/link/controlled-components';
+
+export function useControlCheck<T>({ value, defaultValue, onChange, readOnly }: ControlProps<T>) {
+  const valueInit = useRef(value);
+  const warned = useRef(0);
+
+  useEffect(() => {
+    if (typeof value !== 'undefined') {
+      if ((warned.current & 1) === 0 && typeof valueInit.current === 'undefined') {
+        warned.current |= 1;
+        console.error(error1);
+      }
+
+      if ((warned.current & 2) === 0 && typeof onChange === 'undefined' && typeof readOnly === 'undefined') {
+        warned.current |= 2;
+        console.warn(error2);
+      }
+
+      if ((warned.current & 4) === 0 && typeof defaultValue !== 'undefined') {
+        warned.current |= 4;
+        console.error(error3);
+      }
+    } else {
+      if ((warned.current & 8) === 0 && typeof valueInit.current !== 'undefined') {
+        warned.current |= 8;
+        console.error(error4);
+      }
+    }
+  }, [value, defaultValue, onChange, readOnly]);
+
+  return typeof valueInit.current !== 'undefined';
+}
