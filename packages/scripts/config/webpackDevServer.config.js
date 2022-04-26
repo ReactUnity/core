a96ea2ae1909b4f5de50d56022103e766c284c16
Gokhan Kurt <krtgkn@gmail.com>
2022-04-27 01:25:27 +0300

    disable webpack-dev-server overlay

--- packages/scripts/config/webpackDevServer.config.js
+++ packages/scripts/config/webpackDevServer.config.js
@@ -93,10 +93,7 @@ module.exports = function (proxy, allowedHost) {
         pathname: sockPath,
         port: sockPort,
       },
-      overlay: {
-        errors: true,
-        warnings: false,
-      },
+      overlay: false,
     },
     devMiddleware: {
       // It is important to tell WebpackDevServer to use the same "publicPath" path as
