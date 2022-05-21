39fcdefe7a8aeb22dd2f294c07118a2d02dce7d5
Gokhan Kurt <krtgkn@gmail.com>
2022-05-21 03:16:03 +0300

    improve fireByEventRef native Array/List detection

--- packages/renderer/src/renderer/async/callbacks.ts
+++ packages/renderer/src/renderer/async/callbacks.ts
@@ -1,3 +1,4 @@
+import { System } from '../../models/generated';
 import { AsyncCallback } from './commands';
 import { ObjectsRepo } from './objects';
 
@@ -9,18 +10,28 @@ export class CallbacksRepo extends ObjectsRepo<AsyncCallback> {
   call = (ind: number, args: any[]) => {
     const cb = this.getObject(ind);
 
-    if ('Length' in args) {
-      const newArgs = [];
+    const argsAsList = args as unknown as System.Collections.Generic.List<any>;
+    const argsAsArray = args as unknown as System.Array;
 
-      const length = args['Length'];
-
-      for (let index = 0; index < length; index++) {
-        const element = args[index];
-        newArgs.push(element);
-      }
-
-      args = newArgs;
+    if (typeof argsAsArray.Length === 'number') {
+      // C# Array
+      args = [];
+      const length = argsAsArray.Length;
+      for (let index = 0; index < length; index++) args.push(argsAsArray.GetValue(index));
     }
+    else if (typeof argsAsList.Count === 'number') {
+      // C# List
+      args = [];
+      const length = argsAsList.Count;
+      for (let index = 0; index < length; index++) args.push(argsAsList[index]);
+    }
+    else if (typeof argsAsList.Count === 'function') {
+      // C# IList
+      args = [];
+      const length = (argsAsList.Count as any)();
+      for (let index = 0; index < length; index++) args.push(argsAsArray.GetValue(index));
+    }
+
     return cb.apply(null, args);
   };
 }
