38ef58967e296d0f39abcb1d7ad1baba820b7ffd
dungeon2567 <31074903+dungeon2567@users.noreply.github.com>
2022-09-11 00:17:25 -0300

    Update EndDragHandler.cs

--- Runtime/Frameworks/UGUI/EventHandlers/EndDragHandler.cs
+++ Runtime/Frameworks/UGUI/EventHandlers/EndDragHandler.cs
@@ -5,13 +5,30 @@ using UnityEngine.EventSystems;
 namespace ReactUnity.UGUI.EventHandlers
 {
     [EventHandlerPriority(EventPriority.Discrete)]
-    public class EndDragHandler : MonoBehaviour, IEndDragHandler, IEventHandler
+    public class EndDragHandler : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IEventHandler
     {
         public event Action<BaseEventData> OnEvent = default;
+        
+        private bool IsDragging;
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            IsDragging = true;
+        }
 
         public void OnEndDrag(PointerEventData eventData)
         {
             OnEvent?.Invoke(eventData);
+            
+            IsDragging = false;
+        }
+        
+        private void OnDisable(){
+            if(IsDragging) {
+                OnEvent?.Invoke(eventData);
+                
+                IsDragging = false;
+            }
         }
 
         public void ClearListeners()
