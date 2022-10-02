2beb28b368465c8313d5c7a2e77b230860cdbf43
Gokhan Kurt <krtgkn@gmail.com>
2022-10-02 19:26:47 +0300

    add performance test for bg draw call count

--- Tests/Performance/Base/IntroTests.cs
+++ Tests/Performance/Base/IntroTests.cs
@@ -14,5 +14,31 @@ namespace ReactUnity.Tests.Performance
             yield return Measure.Frames().WarmupCount(10).MeasurementCount(60).Run();
         }
 
+        [UGUITest(Script = @"
+const arr = new Array(200).fill(0);
+
+function App() {
+    return <>
+        {arr.map((x,i) => <bg key={i} />)}
+    </>;
+}
+
+",
+            Style = @"
+:host {
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+
+bg {
+    width: 20px;
+    height: 20px;
+    background-color: yellow;
+}
+"), Performance]
+        public IEnumerator TestFramePerformanceWithMultipleBackgrounds()
+        {
+            yield return Measure.Frames().WarmupCount(10).MeasurementCount(60).ProfilerMarkers("Batches Count").Run();
+        }
     }
 }
