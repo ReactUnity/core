8d1c18575c423d90c50bece83f1cba8f29782ed5
Gokhan Kurt <krtgkn@gmail.com>
2023-11-19 14:12:57 +0300

    allow encoding helpers to handle long strings

--- Runtime/Scripting/DomProxies/EncodingHelpers.cs
+++ Runtime/Scripting/DomProxies/EncodingHelpers.cs
@@ -1,12 +1,25 @@
 using System;
+using System.Text;
 
 namespace ReactUnity
 {
     public static class EncodingHelpers
     {
+        // Uri.Escape methods don't support more than 32766 characters so we split it up
+        private const int EscapeLimit = 32000;
+
         public static string encodeURI(string input)
         {
-            return Uri.EscapeUriString(input);
+            var len = input.Length;
+            if (len < EscapeLimit) return Uri.EscapeUriString(input);
+
+            var res = new StringBuilder();
+            for (int i = 0; i < len; i += EscapeLimit)
+            {
+                var sub = input.Substring(i, Math.Min(len - i, EscapeLimit));
+                res.Append(Uri.EscapeUriString(sub));
+            }
+            return res.ToString();
         }
 
         public static string decodeURI(string input)
@@ -16,7 +29,16 @@ namespace ReactUnity
 
         public static string encodeURIComponent(string input)
         {
-            return Uri.EscapeDataString(input);
+            var len = input.Length;
+            if (len < EscapeLimit) return Uri.EscapeDataString(input);
+
+            var res = new StringBuilder();
+            for (int i = 0; i < len; i += EscapeLimit)
+            {
+                var sub = input.Substring(i, Math.Min(len - i, EscapeLimit));
+                res.Append(Uri.EscapeDataString(sub));
+            }
+            return res.ToString();
         }
 
         public static string decodeURIComponent(string input)
