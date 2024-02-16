d1fea92e224eb2e55ecb22b51a7074b0e620d5bb
Gokhan Kurt <krtgkn@gmail.com>
2024-02-16 03:34:00 +0300

    fix crash on pushing Type to JS

--- unity/quickjs/Runtime/Source/Binding/Values_push_class.cs
+++ unity/quickjs/Runtime/Source/Binding/Values_push_class.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 
 namespace QuickJS.Binding
 {
@@ -54,8 +52,9 @@ namespace QuickJS.Binding
         {
             var context = ScriptEngine.GetContext(ctx);
             var types = context.GetTypeDB();
-            var jsVal = types.GetPrototypeOf(o);
-            return JSApi.JS_DupValue(ctx, jsVal);
+
+            var jsVal = types.GetConstructorOf(o);
+            return jsVal;
         }
 
         public static JSValue js_push_classvalue(JSContext ctx, object o)
