c40f06074070f6e9b7645cd4ea7a70daef07ef2a
Gokhan Kurt <krtgkn@gmail.com>
2024-04-09 16:33:33 +0300

    remove copyright notices

--- Runtime/Yoga/Logger.cs
+++ Runtime/Yoga/Logger.cs
@@ -1,11 +1,4 @@
-﻿/*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-namespace Facebook.Yoga
+﻿namespace Facebook.Yoga
 {
     public delegate void Logger(
         YogaConfig config,
