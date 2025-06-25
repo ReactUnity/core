849829dbcc4c604ed2d70e616a47e90d69b99289
Gokhan Kurt <krtgkn@gmail.com>
2025-06-25 19:38:02 +0300

    update all packages

--- packages/scripts/config/eslintConfig.js
+++ packages/scripts/config/eslintConfig.js
@@ -1,14 +1,4 @@
-// @remove-on-eject-begin
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-// @remove-on-eject-end
-'use strict';
-
-const path = require('path');
+const path = require('node:path');
 const paths = require('./paths');
 const modules = require('./modules');
 
@@ -26,6 +16,5 @@ module.exports = {
   cache: true,
   cacheLocation: path.resolve(paths.appNodeModules, '.cache/.eslintcache'),
   cwd: paths.appPath,
-  resolvePluginsRelativeTo: __dirname,
   baseConfig,
 };
