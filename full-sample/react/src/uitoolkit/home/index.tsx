3677741530905904cc34e861435d84cf20677980
Gokhan Kurt <krtgkn@gmail.com>
2025-06-29 20:33:10 +0300

    add uitoolkit icons

--- full-sample/react/src/uitoolkit/home/index.tsx
+++ full-sample/react/src/uitoolkit/home/index.tsx
@@ -1,6 +1,6 @@
 /* @jsxImportSource @reactunity/renderer/uitoolkit */
 
-import { render } from '@reactunity/renderer';
+import { icon, render } from '@reactunity/renderer';
 import base64Image from 'src/assets/base64Image.txt';
 import pngImage from 'src/assets/bg.png';
 import check, { ReactComponent as CheckSVG } from 'src/assets/check.svg';
@@ -9,56 +9,66 @@ import style from './index.module.scss';
 const webImage = 'https://www.google.com.tr/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
 
 export function App() {
-  return <scroll>
-    <view className={style.app}>
-      <h1>React Unity Showcase</h1>
+  return (
+    <scroll>
+      <view className={style.app}>
+        <h1>React Unity Showcase</h1>
 
-      <CheckSVG />
-      <svg source={check} />
-      <svg source={'https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/betterplace.svg'} />
-      <svg source={'https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/compuserver_msn_Ford_Focus.svg'} />
+        <CheckSVG />
+        <svg source={check} />
+        <svg source={'https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/betterplace.svg'} />
+        <svg source={'https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/compuserver_msn_Ford_Focus.svg'} />
 
-      <section>
-        <h2>Button</h2>
+        <section>
+          <h2>Button</h2>
 
-        <button className={style.clickButton}>Click</button>
-      </section>
+          <button className={style.clickButton}>Click</button>
+        </section>
 
+        <section>
+          <h2>Anchor</h2>
 
-      <section>
-        <h2>Anchor</h2>
+          <anchor url="https://www.google.com">Open Google</anchor>
+        </section>
 
-        <anchor url="https://www.google.com">Open Google</anchor>
-      </section>
+        <section>
+          <h2>Input</h2>
 
+          <input />
+        </section>
 
-      <section>
-        <h2>Input</h2>
+        <section>
+          <h2>Toggle</h2>
 
-        <input />
-      </section>
+          <row>
+            <toggle />
+            Toggle
+          </row>
+        </section>
 
+        <section>
+          <h2>Image</h2>
 
-      <section>
-        <h2>Toggle</h2>
+          <row>
+            <image source={pngImage} />
+            <image source={base64Image} />
+            <image source={webImage} />
+          </row>
+        </section>
 
-        <row>
-          <toggle />
-          Toggle
-        </row>
-      </section>
+        <section>
+          <h2>Icons</h2>
 
-      <section>
-        <h2>Image</h2>
-
-        <row>
-          <image source={pngImage} />
-          <image source={base64Image} />
-          <image source={webImage} />
-        </row>
-      </section>
-    </view>
-  </scroll>;
-};
+          <row>
+            <icon.image />
+            <icon.person_add />
+            <icon.hourglass_bottom />
+            <icon._3d_rotation style={{ fontSize: 40 }} />
+          </row>
+        </section>
+      </view>
+    </scroll>
+  );
+}
 
 render(<App />);
